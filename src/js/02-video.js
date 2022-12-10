c484af9c74c636743c46b731df482873601fe92f
Katya-Semeniuk <katyasemeniuk@gmail.com>
2022-12-10 23:03:03 +0100

    fixed mistakes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,21 +5,19 @@ import throttle from 'lodash.throttle';
 const player = new Player(iframe);
 
 function videoTime(data) {
-    localStorage.setItem('videoplayer-current-time', 1000)
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
 };
-
-// videoTime(data)
 
 player.on('timeupdate', throttle(videoTime, 1000)) ;
 
 
  function checkcurrentTime() {
-     const currentTime = localStorage.getItem('videoplayer-current-time')
-     const parseCurrentTime = JSON.parse(currentTime)
-     
+     const currentTime = localStorage.getItem('videoplayer-current-time');
+     const parseCurrentTime = JSON.parse(currentTime);
   
-if (!parseCurrentTime)
-    return
- }
- player.setCurrentTime(parseCurrentTime)
+     if (!parseCurrentTime)
+         return
+     player.setCurrentTime(parseCurrentTime);
+};
+ 
 checkcurrentTime()
